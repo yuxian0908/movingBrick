204df0ece7f37c0c28175e06d399ffaa07901913
yuxian <andyretajeff@gmail.com>
2018-05-24 02:07:49 +0800

    get data test ok

--- helper/bitopro.py
+++ helper/bitopro.py
@@ -9,55 +9,76 @@ import urllib
 import pytesseract
 from PIL import Image
 
+class bitoproHelper:
+    def __init__(self):
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        browser = webdriver.Chrome(chrome_options=option)
+        # browser = webdriver.Chrome()
+        self.browser = browser
+        return
 
-def totp():
-    totp = pyotp.TOTP("SMWULMNPRNJOQEWW") 
-    print(totp.now())
-    return totp.now()
+    def totp(self):
+        totp = pyotp.TOTP("SMWULMNPRNJOQEWW") 
+        print(totp.now())
+        return totp.now()
 
-def login():
-    LOGIN_URL = "https://www.bitopro.com/"
-    USEREMAIL = 'yuxian12070908@gmail.com'
-    PASSWORD = 'Andy551209'
+    def login(self):
+        LOGIN_URL = "https://www.bitopro.com/"
+        USEREMAIL = 'yuxian12070908@gmail.com'
+        PASSWORD = 'Andy551209'
 
-    option = webdriver.ChromeOptions()
-    option.add_argument('headless')
-    browser = webdriver.Chrome(chrome_options=option)
-    # browser = webdriver.Chrome()
+        
+        browser = self.browser
 
-    # enter loginin div
-    browser.get(LOGIN_URL)
-    browser.find_element_by_xpath(".//*[@id='navbar']/ul/*[3]/a").click()
+        # enter loginin div
+        browser.get(LOGIN_URL)
+        browser.find_element_by_xpath(".//*[@id='navbar']/ul/*[3]/a").click()
 
-    # find verify code img url and open it in another browser
-    time.sleep(1)  
-    imgURL = browser.find_element_by_xpath(".//*[@id='captcha_key']/../*[1]").get_attribute("src")
-    option = webdriver.ChromeOptions()
-    option.add_argument('headless')
-    browser2 = webdriver.Chrome(chrome_options=option)
-    # browser2 = webdriver.Chrome()    
-    browser2.get(imgURL)
-    browser2.save_screenshot('vericode.jpg')
-    time.sleep(1) 
-    img = Image.open('vericode.jpg')
-    img.show()
-    
-    # type in the code
-    print('type in the code you see')
-    vericode = input("code:")
-    browser2.close()
+        # find verify code img url and open it in another browser
+        time.sleep(1)  
+        imgURL = browser.find_element_by_xpath(".//*[@id='captcha_key']/../*[1]").get_attribute("src")
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        browser2 = webdriver.Chrome(chrome_options=option)
+        # browser2 = webdriver.Chrome()    
+        browser2.get(imgURL)
+        browser2.save_screenshot('vericode.jpg')
+        time.sleep(1) 
+        img = Image.open('vericode.jpg')
+        img.show()
+        
+        # type in the code
+        print('type in the code you see')
+        vericode = input("code:")
+        browser2.close()
 
-    # auto enter input
-    browser.find_element_by_xpath(".//*[@id='loginForm']/div/input[@name='email']").send_keys(USEREMAIL)
-    browser.find_element_by_xpath(".//*[@id='loginForm']/div/input[@name='password']").send_keys(PASSWORD)
-    browser.find_element_by_xpath(".//*[@id='captcha']").send_keys(vericode)
+        # auto enter input
+        browser.find_element_by_xpath(".//*[@id='loginForm']/div/input[@name='email']").send_keys(USEREMAIL)
+        browser.find_element_by_xpath(".//*[@id='loginForm']/div/input[@name='password']").send_keys(PASSWORD)
+        browser.find_element_by_xpath(".//*[@id='captcha']").send_keys(vericode)
 
-    # submit form
-    browser.find_element_by_xpath(".//*[@id='login-btn']").click()
+        # submit form
+        try:
+            browser.find_element_by_xpath(".//*[@id='login-btn']").click()
+        except:
+            print('you need to rerun again')
+            return
 
-    # get opt
-    time.sleep(1)  
-    browser.find_element_by_xpath(".//*[@id='otp']").send_keys(totp())
+        # get opt
+        time.sleep(1)  
+        try:
+            browser.find_element_by_xpath(".//*[@id='otp']").send_keys(self.totp())
+        except:
+            print('you need to rerun again')
+            return
+        time.sleep(1)  
+        
+        
 
-    return browser
-    
+    def getTick(self,lookingCoinType):
+        if lookingCoinType=="btc":
+            browser = self.browser
+            test = browser.find_element_by_xpath(".//*[@id='quote']").get_attribute("class")
+            print(test)
+            return "aaa"
--- tradePlace/bitopro.py
+++ tradePlace/bitopro.py
@@ -8,15 +8,15 @@ from selenium.webdriver.common.keys import Keys
 import urllib
 
 from .tradePlace import tradePlace
-import helper.bitopro as helper
+from helper.bitopro import bitoproHelper
 
 class bitopro(tradePlace):
     def __init__(self, lookingDataType, lookingCoinType):
+        self.helper = self.login()
         self.lookingDataType = lookingDataType
         self.lookingCoinType = lookingCoinType
  
     def router(self):
-        self.browser = self.login()
         Data = {
             "Tick" : lambda: self.Tick(),
             "Bid"  : lambda: self.Bid(),
@@ -28,29 +28,13 @@ class bitopro(tradePlace):
 
 
     def Tick(self):
-        currentTime = int(time.time())
-        beginTime = str(currentTime-1000)
-        currentTime = str(currentTime)
-        cookie = {}
-
-        session = requests.Session()
 
         if self.lookingCoinType=="btc":
-            #BTC
-            cookie = {'session': '51731c9f09d1517e485bfc55696126d4'}
+            return self.helper.getTick(self.lookingCoinType)
         elif self.lookingCoinType=="ltc":
-            #LTC
-            cookie = {'session': '6012a6d6586b0ae11feb20cec83a662d'}
+            return "btcData"
         elif self.lookingCoinType=="eth":
-            #ETH
-            cookie = {'session': '5ef3f4f26870e21e53ad5674d475b444'}
-
-        btcToTwd = session.get("https://www.bitopro.com/trading_datas/history?symbol="+
-                                    self.lookingCoinType+"&resolution=180&from="+
-                                    beginTime+"&to="+currentTime,cookies=cookie)
-        btcToTwd = json.loads(btcToTwd.text)
-
-        return btcToTwd[0][4]
+            return "btcData"
 
 
     def Bid(self):
@@ -72,4 +56,6 @@ class bitopro(tradePlace):
 
 
     def login(self):
-        return helper.login()
+        helper = bitoproHelper()
+        helper.login()
+        return helper
